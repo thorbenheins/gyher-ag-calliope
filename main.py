517e4e4132ad2e0817acc896bd4effe6706e48b3
thorbenheins <thorbenheins@users.noreply.github.com>
2023-09-14 11:36:12 +0200

    02: lösung mit variablen

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-let x = 0
-let y = 0
-basic.forever(function () {
+x = 0
+y = 0
+
+def on_forever():
+    global x, y
     led.plot(x, y)
     basic.pause(100)
     led.unplot(x, y)
-    if (x < 4) {
+    if x < 4:
         x = x + 1
-    } else if (x == 4) {
+    elif x == 4:
         x = 0
         y = y + 1
-    }
-    if (y == 5) {
+    if y == 5:
         y = 0
-    }
-})
+basic.forever(on_forever)
