8519e855945584c7c4e9ac974b4be875192c8e64
thorbenheins <thorbenheins@users.noreply.github.com>
2023-09-21 15:02:18 +0200

    2: alternative lösung, besser lesbar

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ def on_forever():
     led.plot(x, y)
     basic.pause(100)
     led.unplot(x, y)
-    if x < 4:
+
+    if (x < 5):
         x = x + 1
-    elif x == 4:
+
+    if (x == 5):
         x = 0
         y = y + 1
-    if y == 5:
+
+    if (y == 5):
         y = 0
+   
 basic.forever(on_forever)
